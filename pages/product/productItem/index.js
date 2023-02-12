1cdb8d9f8a171f1e175fc7cefe108981f0f6130a
31011972Qw <ggtrfr@bk.ru>
2023-02-12 12:43:01 +0300

    items

--- pages/product/productItem/index.js
+++ pages/product/productItem/index.js
@@ -5,7 +5,12 @@ import styles from "./productItem.module.css";
 const ProductItem = ({obj}) => {
   return (
     <div className={styles.item}>
-
+      <Image
+        src="/unsplash_sxiSod0tyYQ.png"
+        alt="Логотип"
+        width={380}
+        height={215}
+      />
       <div className={styles.description}>
         <h5>{obj.title}</h5>
         <p>
